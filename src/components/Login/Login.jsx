8cbd0f7b1c012e9a0be3fb73efe3c10ecc559983
ednesor <edgar.funes2001@gmail.com>
2023-01-11 21:55:56 -0300

    feat: add sections of login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Login(){
+    return(
+        <div>
+            <div className="profiles">
+
+            </div>
+            <div className="loginForm">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    )
+}
