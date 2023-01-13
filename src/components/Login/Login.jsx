788d080adbdddab60f4f95ceb1fb6b5cc0cedf9d
ednesor <edgar.funes2001@gmail.com>
2023-01-12 23:18:44 -0300

    feat: styled components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Login(){
-    return(
-        <div>
-            <div className="profiles">
+const Logo = styled("img")({
+    width: "200px",
+    marginBottom: "-35px"
+})
+const Title = styled("p")({
+    fontSize: "25px",
+})
+const Container = styled("div")({
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    paddingTop: "8px",
+})
+const Profiles = styled("div")({
 
-            </div>
+})
+
+export default function Login() {
+    return (
+        <Container>
+            <Profiles>
+                <div>
+                    <Logo src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="facebook" />
+                    <Title>Inicios de sesión recientes</Title>
+                    <p>Haz clic en tu foto o agrega una cuenta</p>
+                </div>
+                <div>
+                    <div>Perfil</div>
+                </div>
+            </Profiles>
             <div className="loginForm">
 
             </div>
             <div className="footer">
 
             </div>
-        </div>
+        </Container>
     )
 }
