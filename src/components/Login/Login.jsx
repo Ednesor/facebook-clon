11b56e00c36be6a6fdca3c394410e63bba751968
ednesor <edgar.funes2001@gmail.com>
2023-01-13 18:19:54 -0300

    style: session

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,10 +3,12 @@ import styled from "styled-components";
 
 const Logo = styled("img")({
     width: "200px",
-    marginBottom: "-35px"
+    marginBottom: "-31px",
+    marginLeft: "-20px"
 })
 const Title = styled("p")({
     fontSize: "25px",
+    marginBottom: "-14px"
 })
 const Container = styled("div")({
     display: "flex",
@@ -14,23 +16,52 @@ const Container = styled("div")({
     justifyContent: "center",
     paddingTop: "8px",
 })
-const Profiles = styled("div")({
+const SubTitle = styled("p")({
+    color: "#5F5F5F"
+})
+const Sesion = styled("div")({
+
+})
+const  AddAccount = styled("div")({
+    marginTop: "26px",
+    backgroundColor: "#F5F6F7",
+    height: "206px",
+    border: "1px solid #DCDCDC",
+    borderRadius: "10px",
+    width: "160px",
+    boxShadow: "0px 0px 20px #F5F6F7",
+    transition: "all .25s",
+    "&:hover":{
+        boxShadow: "0px 0px 20px #CBCCCC"
+    }
+})
+const Name = styled("p")({
+    color: "#1976F2",
+    fontSize: "20px",
+    width: "100%",
+    textAlign: "center",
+    top: "0px",
+})
+const Add = styled("div")({
 
 })
 
 export default function Login() {
     return (
         <Container>
-            <Profiles>
+            <Sesion>
                 <div>
                     <Logo src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="facebook" />
                     <Title>Inicios de sesión recientes</Title>
-                    <p>Haz clic en tu foto o agrega una cuenta</p>
+                    <SubTitle>Haz clic en tu foto o agrega una cuenta</SubTitle>
                 </div>
                 <div>
-                    <div>Perfil</div>
+                    <AddAccount>
+                        <Add>Boton</Add>
+                        <Name>Agregar cuenta</Name>
+                    </AddAccount>
                 </div>
-            </Profiles>
+            </Sesion>
             <div className="loginForm">
 
             </div>
