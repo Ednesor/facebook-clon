c04dc0ed96a4fe88369e17b405fb8032db950dd6
ednesor <edgar.funes2001@gmail.com>
2023-01-18 20:28:29 -0300

    feat: add navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 export default function Home(){
     return(
-        <div>home</div>
+        <div>
+            <Navbar />
+        </div>
     )
 }
