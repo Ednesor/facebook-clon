795657774f0d3d4aa46e0e56b5661a700564dc72
ednesor <edgar.funes2001@gmail.com>
2023-01-18 17:38:16 -0300

    feat: add Home route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={<Login />} />
-        <Route exact path='/home' element={<Login />} />
+        <Route exact path='/home' element={<Home />} />
       </Routes>
     </Router>
   );
