9378f5916668e0e40eefe22efaa15b1f7d0de571
ednesor <edgar.funes2001@gmail.com>
2023-01-18 02:34:32 -0300

    style: sesion responsive

--- src/components/Login/Sesion.jsx
+++ src/components/Login/Sesion.jsx
@@ -5,13 +5,19 @@ import { BsFillPlusCircleFill } from "react-icons/bs"
 const Logo = styled("img")({
     width: "200px",
     marginBottom: "-31px",
-    marginLeft: "-20px"
+    marginLeft: "-20px",
+    '@media(max-width: 1000px)': {
+        marginLeft: "0px"
+    },
 })
 const Title = styled("p")({
     fontSize: "30px",
     marginBottom: "-14px",
     fontWeight: 100,
-    color: "#201D1C"
+    color: "#201D1C",
+    '@media(max-width: 1000px)': {
+        textAlign: "center",
+    }
 })
 
 const SubTitle = styled("p")({
@@ -53,24 +59,66 @@ const Add = styled(BsFillPlusCircleFill)({
     marginTop: "16px",
 })
 const Accounts = styled("div")({
-    userSelect: "none"
+    userSelect: "none",
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    width: "340px",
+    '@media(max-width: 1000px)': {
+        justifyContent: "space-around",
+    }
+})
+const Container = styled("div")({
+    display: "flex",
+    justifyContent: "center",
+    '@media(max-width: 1000px)': {
+        width: "100%",
+        maxWidth: "500px"
+    }
+})
+const SubContainer = styled("div")({
+    width: "350px",
+    '@media(max-width: 1000px)': {
+        width: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+    }
+})
+const SubCont2 = styled("div")({
+    '@media(max-width: 1000px)': {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center"
+    }
 })
 
 
 export default function Sesion() {
     return (
-        <div>
-            <div>
-                <Logo src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="facebook" />
-                <Title>Inicios de sesión recientes</Title>
-                <SubTitle>Haz clic en tu foto o agrega una cuenta</SubTitle>
-            </div>
-            <Accounts>
-                <AddAccount>
-                    <Add size={42} />
-                    <Name>Agregar cuenta</Name>
-                </AddAccount>
-            </Accounts>
-        </div>
+        <Container>
+            <SubCont2>
+                <SubContainer>
+                    <Logo src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="facebook" />
+                    <Title>Inicios de sesión recientes</Title>
+                    <SubTitle>Haz clic en tu foto o agrega una cuenta</SubTitle>
+                </SubContainer>
+                <Accounts>
+                    <AddAccount>
+                        <Add size={42} />
+                        <Name>Agregar cuenta</Name>
+                    </AddAccount>
+                    <AddAccount>
+                        <Add size={42} />
+                        <Name>Agregar cuenta</Name>
+                    </AddAccount>
+                    <AddAccount>
+                        <Add size={42} />
+                        <Name>Agregar cuenta</Name>
+                    </AddAccount>
+                    
+                </Accounts>
+            </SubCont2>
+        </Container>
     )
 }
