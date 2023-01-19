01aa3fe56b1609454255be812cf4b5eafefc43c0
ednesor <edgar.funes2001@gmail.com>
2023-01-19 01:07:35 -0300

    feat: add navbar route buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 import styled from "styled-components";
-import { AiFillHome } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import { MdHome } from "react-icons/md";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsPeopleFill } from "react-icons/bs";
+import { BsPeople } from "react-icons/bs";
+import { BsDisplay } from "react-icons/bs";
+import { BsDisplayFill } from "react-icons/bs";
+import { IoStorefrontSharp } from "react-icons/io5";
+import { IoStorefrontOutline } from "react-icons/io5";
+import { IoGameControllerSharp } from "react-icons/io5";
+import { IoGameControllerOutline } from "react-icons/io5";
 
+const styleIconsOut = {
+    color: "#B8BBBF",
+    paddingBottom: "10px",
+    width: "100%",
+    paddingTop: "5px",
+    marginBottom: "4px",
+    "&:hover": {
+        backgroundColor: "#3A3B3C",
+        borderRadius: "10px",
+    }
+}
+const styleIcons = {
+    color: "#2694FF",
+    borderBottom: "solid 3px #2694FF",
+    paddingBottom: "10px",
+    width: "100%",
+}
+const FriendsIcon = styled(BsPeopleFill)(styleIcons)
+const FriendsOut = styled(BsPeople)(styleIconsOut)
+const HomeIcon = styled(MdHome)(styleIcons)
+const HomeOut = styled(AiOutlineHome)(styleIconsOut)
+const WatchIcon = styled(BsDisplayFill)(styleIcons)
+const WatchOut = styled(BsDisplay)(styleIconsOut)
+const MarketIcon = styled(IoStorefrontSharp)(styleIcons)
+const MarketOut = styled(IoStorefrontOutline)(styleIconsOut)
+const GameIcon = styled(IoGameControllerSharp)(styleIcons)
+const GameOut = styled(IoGameControllerOutline)(styleIconsOut)
+
+
+const Link = styled(NavLink)({
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-end",
+    width: "110px",
+    "&:hover::before": {
+        content: "algo",
+        backgroundColor: "#C8C8C8",
+        position: "fixed"
+    }
+})
 const Container = styled("div")({
     backgroundColor: "#242526",
     height: "55px",
@@ -9,9 +59,10 @@ const Container = styled("div")({
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
+    position: "fixed",
+    width: "100%",
 })
 const Header = styled("div")({
-    // backgroundColor: "red",
     width: "295px",
     display: "flex",
     justifyContent: "space-around",
@@ -29,6 +80,7 @@ const Search = styled("input")({
     backgroundColor: "#3A3B3C",
     fontSize: "14px",
     paddingLeft: "15px",
+    color: "white",
     "&::placeholder": {
         color: "#9EA0A2",
     },
@@ -37,6 +89,11 @@ const Search = styled("input")({
     }
 })
 const Sections = styled("div")({
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-end",
+})
+const Menu = styled("div")({
 
 })
 
@@ -48,8 +105,35 @@ export default function Navbar() {
                 <Search type="text" name="search" id="search" placeholder="Buscar en Facebook" />
             </Header>
             <Sections>
-
+                <Link to="/">
+                    {({ isActive }) => (
+                        isActive ? <HomeIcon size={30} /> : <HomeOut size={30} />
+                    )}
+                </Link>
+                <Link to="/friends">
+                    {({ isActive }) => (
+                        isActive ? <FriendsIcon size={30} /> : <FriendsOut size={30} />
+                    )}
+                </Link>
+                <Link to="/watch">
+                    {({ isActive }) => (
+                        isActive ? <WatchIcon size={30} /> : <WatchOut size={30} />
+                    )}
+                </Link>
+                <Link to="/market">
+                    {({ isActive }) => (
+                        isActive ? <MarketIcon size={30} /> : <MarketOut size={30} />
+                    )}
+                </Link>
+                <Link to="/game">
+                    {({ isActive }) => (
+                        isActive ? <GameIcon size={30} /> : <GameOut size={30} />
+                    )}
+                </Link>
             </Sections>
+            <Menu>
+                
+            </Menu>
         </Container>
     )
 }
