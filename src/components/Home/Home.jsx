ee5d23a47688a11ec6d7624c9b89777b4a6cb0c3
ednesor <edgar.funes2001@gmail.com>
2023-01-23 18:30:46 -0300

    Refactor: folders

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import HomePage from "./HomePage/HomePage";
+import MenuOptions from "./MenuOptions";
+import RightMenu from "./RightMenu";
 
-export default function Home(){
-    return(
-        <div>
-            home
-        </div>
+const Container = styled("div")({
+    backgroundColor: "#18191A",
+    paddingTop: "60px",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    height: "100vh"
+});
+
+export default function Home() {
+    return (
+        <Container>
+            <MenuOptions />
+            <HomePage />
+            <RightMenu />
+        </Container>
     )
 }
